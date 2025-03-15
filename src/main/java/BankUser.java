a66d26b4d0d3d5dc1f87ed7c7584a9f0d874064f
RawandoS <nicholasgardin07@gmail.com>
2025-03-15 21:19:37 +0100

    renamed variables

--- src/main/java/BankUser.java
+++ src/main/java/BankUser.java
@@ -96,7 +96,7 @@ public class BankUser {
     System.out.println("Time spent: " + timeSpent);
   }
 
-  void monthBonus() {
+  public void monthBonus() {
     wallet += 100;
     System.out.println("\nA month has passed, 100€ have been deposited in your wallet.");
     try {
@@ -108,79 +108,71 @@ public class BankUser {
     }
   }
 
-  void moveForward(int t) {
+  public void moveForward(int t) {
     timeSpent += t;
     for (int i = 0; i < t / 30; i++) {
       this.monthBonus();
     }
   }
 
-  void investment(double amount, char period, char risk) {
+  public void investment(double initialAmount, char period, char risk) {
     Random rand = new Random();
-    if (amount > balance || balance <= 0) {
+    if (initialAmount > balance || balance <= 0) {
       System.out.println("Insufficient founds");
       return;
     }
-    balance -= amount;
-    int _period = 0;
+    balance -= initialAmount;
+    int time = 0;
     if (period == 's') {
-      _period = 30;
+      time = 30;
     } else if (period == 'm') {
-      _period = 180;
+      time = 180;
     } else if (period == 'l') {
-      _period = 360;
+      time = 360;
     } else {
       System.out.println("Invalid period.");
       return;
     }
-    double _risk = 0;
+    double multiplier = 0;
     if (risk == 'l') {
-      _risk = amount + amount * 2;
+      multiplier = initialAmount + initialAmount * 2;
     } else if (risk == 'm') {
-      _risk = amount + amount * 5;
+      multiplier = initialAmount + initialAmount * 5;
     } else if (risk == 'h') {
-      _risk = amount + amount * 10;
+      multiplier = initialAmount + initialAmount * 10;
     } else {
       System.out.println("Invalid risk.");
       return;
     }
-    double _amount = amount;
-    timeSpent += _period;
-    for (int i = 1; i <= _period; i++) {
-      _amount = rand.nextInt(1000000) % _risk;
+    double finalAmount = initialAmount;
+    timeSpent += time;
+    for (int i = 1; i <= time; i++) {
+      finalAmount = rand.nextInt(1000000) % multiplier;
       if (rand.nextInt(1000000) % 2 != 0) {
-        _amount *= -1;
+        finalAmount *= -1;
       }
       if (i % 30 == 0) {
         this.monthBonus();
       }
     }
-    if (risk == 'l' && (_amount < 0.7 * amount)) {
-      _amount = 0.7 * amount;
-    } else if (risk == 'm' && (_amount < 0.4 * amount)) {
-      _amount = 0.4 * amount;
+    if (risk == 'l' && (finalAmount < 0.7 * initialAmount)) {
+      finalAmount = 0.7 * initialAmount;
+    } else if (risk == 'm' && (finalAmount < 0.4 * initialAmount)) {
+      finalAmount = 0.4 * initialAmount;
     }
-    System.out.print("Initial amount invested: ");
-    System.out.print(amount);
-    System.out.println("€");
-    System.out.print("Final amount: ");
-    System.out.print(_amount);
-    System.out.println("€");
-    if (_amount > amount) {
-      System.out.print("Earned amount: ");
-      System.out.print(_amount - amount);
-      System.out.println("€");
-    } else if (_amount < amount) {
-      System.out.print("Lost amount: ");
-      System.out.print(amount - _amount);
-      System.out.println("€");
+    System.out.print("Initial amount invested: " + initialAmount + "€");
+    System.out.print("Final amount: " + finalAmount + "€");
+    if (finalAmount > initialAmount) {
+      System.out.print("Earned amount: " + (finalAmount - initialAmount) + "€");
+    } else if (finalAmount < initialAmount) {
+      System.out.print("Lost amount: " + (initialAmount - finalAmount) + "€");
     } else {
       System.out.println("No amount earned");
     }
-    balance += _amount;
+    balance += finalAmount;
     try {
       FileWriter writer = new FileWriter(userName + ".txt", true);
-      writer.write("investment;" + _amount + "\n");
+      writer.write("investment;" + finalAmount + "\n");
       writer.close();
     } catch (Exception e) {
       throw new RuntimeException(e);
